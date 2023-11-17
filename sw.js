b5d5c01f07add27823641b96915de849481f8c05
Bypassi <derin@0x44.xyz>
2023-11-17 13:42:33 -0500

    fix and clean up code

--- sw.js
+++ sw.js
@@ -1,42 +1,29 @@
+const target = { targetId: 'browser' }
 
-chrome.runtime.onMessage.addListener(async function (msg, sender, sendResponse) {
-    if (msg.type === "loadinspectargets") {
-        await chrome.debugger.attach({ targetId: "browser" }, '1.3');
-        var allTargets = await chrome.debugger.sendCommand({ targetId: 'browser' }, 'Target.getTargets');
-        await chrome.debugger.detach({ targetId: 'browser' });
-        sendResponse({
-            data: allTargets.targetInfos,
-            type: 'response'
-        });
-    }
-    if (msg.type === "startinspect") {
-        await chrome.debugger.attach({ targetId: 'browser' }, '1.3')
-        function chromeTabsScriptFull() {
-            var url = URL.createObjectURL(new Blob(["<script>alert(1)</script>"], { type: 'text/html' }));
-            chrome.tabs.create({ url: url });
-        }
-        const code =  function () {
-            alert("wassup");
-        }
-        chrome.debugger.onEvent.addListener(async function (e, v, p) {
-            var reqID = p.requestId;
-            var isGoguardian = p.request.url.includes("haldl");
-            if (isGoguardian) {
-                await chrome.debugger.sendCommand({targetId: "browser"}, "Fetch.fullfillRequest", {
-                    requestId: reqID,
-                    responseCode: 200,
-                    body: atob(`(${code.toString()})()`)
-                })
-                
-            }
-        })
-        await chrome.debugger.sendCommand({targetId: 'browser'}, 'Fetch.enable');
+function injection() {
+    alert("Yo!!");
+}
 
+async function onEvent(_, _, event) {
+    if (!event.request.url.startsWith("chrome-extension://")) return;
 
+    await chrome.debugger.sendCommand(target, "Fetch.fulfillRequest", {
+        requestId: event.requestId,
+        responseCode: 200,
+        body: btoa(`(${injection.toString()})()`)
+    })
+}
 
-    }
-    if (msg.type === "cancelinspect") {
-        await chrome.debugger.detach({ targetId: 'browser' });
-        sendResponse({ error: null, type: 'response', data: {} });
-    }
-})
+async function start() {
+    await chrome.debugger.attach(target, '1.3');
+    // let { targetInfos } = await chrome.debugger.sendCommand(target, 'Target.getTargets');
+
+    chrome.debugger.onEvent.addListener(onEvent)
+    await chrome.debugger.sendCommand(target, 'Fetch.enable');
+}
+
+async function stop() {
+    await chrome.debugger.detach(target);
+}
+
+start();
