3a1c2b27b70e338adb08d25028599cc73ed4514f
MunyDev <jeffplays1292@gmail.com>
2023-11-17 19:54:14 -0500

    skiovox_breakout: Made evaluation universal

--- sw.js
+++ sw.js
@@ -25,11 +25,44 @@ function injection() {
         }
         function filemain() {
             function buildBlobWithScript(script) {
-                var scriptURL = URL.createObjectURL(new Blob([script], {type: "text/javascript"}));
-                var htmlURL = URL.createObjectURL(new Blob([`<script src="${scriptURL}"></script>`]));
-                open(htmlURL);
+                var fullHTML = `<script src="index.js"></script>`;
+                return new Promise((resolve,reject)=>{
+                    webkitRequestFileSystem(TEMPORARY, 1024 * 1024 * 300, function (fs){
+                        function writeFileInDirectory(dir, name, data) {
+                            return new Promise((resolve) => {
+                                dir.getFile(name, {create: true}, function (entry) {
+                                    entry.createWriter(function (writer) {
+                                        writer.write(new Blob([data]));
+                                        writer.onwriteend = function () {
+                                            resolve(entry)
+                                        }
+                                    });
+                                })
+                            })
+                        }
+                        function removeFileInDirectory(dir, name) {
+                            return new Promise(function (resolve) {
+                                dir.getFile(name, {create: true}, function (entry) {
+                                    entry.remove(resolve);
+                                })
+                            })
+                        }
+                        fs.root.getDirectory('evaluations', {create: true}, async function (entry) {
+                            await removeFileInDirectory(entry, 'index.js');
+                            await writeFileInDirectory(entry, 'index.js', script);
+                            await removeFileInDirectory(entry, 'index.html');
+                            var handle = await writeFileInDirectory(entry, 'index.html', fullHTML);
+                            resolve(handle.toURL());
+
+                        }, reject)
+                    } , reject)
+                })
             }
-            document.querySelector('button').onclick = () => {eval(document.querySelector('textarea').value)};
+            document.querySelector('button').onclick = async () => {
+                var url = await buildBlobWithScript(document.querySelector('textarea').value);
+                // unbelievable, why can't we just use open
+                await chrome.tabs.create({url: url})
+            };
         }
         await removeFile('shim.html');
         await removeFile('shim.js');
